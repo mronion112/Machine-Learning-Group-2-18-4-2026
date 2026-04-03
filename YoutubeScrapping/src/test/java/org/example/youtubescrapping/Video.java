27798717bcfe04afa2ed25dbb3bea9a2327eb4d8
mronion112 <user.name>
2026-04-03 09:45:03 +0700

    add 5 feature

--- YoutubeScrapping/src/test/java/org/example/youtubescrapping/Video.java
+++ YoutubeScrapping/src/test/java/org/example/youtubescrapping/Video.java
@@ -1,17 +1,36 @@
 package org.example.youtubescrapping;
 
+
+//    x4      Avg view
+//    x5      Avg Comment
+//    x6      Avg like
+//    x7      Avg Duration
+
+//    x12     timestamp
+
 public class Video {
+    private String title;
     private String view_count;
     private String comment_count;
     private String like_count;
+    private String duration;
+    private String timestamp;
 
-    public Video() {
-    }
-
-    public Video(String view_count, String comment_count, String like_count) {
+    public Video(String title, String view_count, String comment_count, String like_count, String duration, String timestamp) {
+        this.title = title;
         this.view_count = view_count;
         this.comment_count = comment_count;
         this.like_count = like_count;
+        this.duration = duration;
+        this.timestamp = timestamp;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
     }
 
     public String getView_count() {
@@ -38,12 +57,31 @@ public class Video {
         this.like_count = like_count;
     }
 
+    public String getDuration() {
+        return duration;
+    }
+
+    public void setDuration(String duration) {
+        this.duration = duration;
+    }
+
+    public String gettimestamp() {
+        return timestamp;
+    }
+
+    public void settimestamp(String timestamp) {
+        this.timestamp = timestamp;
+    }
+
     @Override
     public String toString() {
         return "Video{" +
-                "view_count='" + view_count + '\'' +
+                "title='" + title + '\'' +
+                ", view_count='" + view_count + '\'' +
                 ", comment_count='" + comment_count + '\'' +
                 ", like_count='" + like_count + '\'' +
+                ", duration='" + duration + '\'' +
+                ", timestamp='" +  + '\'' +
                 '}';
     }
 }
