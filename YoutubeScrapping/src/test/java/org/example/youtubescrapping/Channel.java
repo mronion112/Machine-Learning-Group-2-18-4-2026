455470d01c44ab5558bffb5cfb178bf80dd24b47
mronion112 <user.name>
2026-04-03 09:45:44 +0700

    add 6 feature

--- YoutubeScrapping/src/test/java/org/example/youtubescrapping/Channel.java
+++ YoutubeScrapping/src/test/java/org/example/youtubescrapping/Channel.java
@@ -4,28 +4,52 @@ import java.util.ArrayList;
 
 public class Channel {
 
+//    x1      Channel Follower Count
+//    x2      Epoch
+//    x3      Total Videos
+//    x8      Avg view 10 videos of channel
+//    x9      Avg view 10 videos of channel
+//    x10      Avg like 10 videos of channel
+//    x11     Avg comment 10 videos of channel
+//    x12     Avg Duration 10 videos of Channel
+//    x13     Frequency
+//    x14     isChannelVerify
 
-    String uploader;
+
+
+    String channel;
     String epoch;
     String channel_follower_count;
     String playlist_count;
+    String avgLike10Videos;
+    String avgView10Videos;
+    String avgComment10Videos;
+    String avgDuration10Videos;
+    String freequency;
+    String isChannelVerify;
     ArrayList<Video> videos;
 
-    public Channel(String uploader, String epoch, String channel_follower_count, String playlist_count, ArrayList<Video> listVideos) {
-        this.uploader = uploader;
+
+    public Channel(String channel, String epoch, String channel_follower_count, String playlist_count, String avgView10Videos, String avgLike10Videos, String avgComment10Videos, String avgDuration10Videos, String freequency, String isChannelVerify, ArrayList<Video> videos) {
         this.epoch = epoch;
+        this.channel = channel;
         this.channel_follower_count = channel_follower_count;
-        this.videos = videos;
         this.playlist_count = playlist_count;
-        this.videos = listVideos;
+        this.avgView10Videos = avgView10Videos;
+        this.avgLike10Videos = avgLike10Videos;
+        this.avgComment10Videos = avgComment10Videos;
+        this.avgDuration10Videos = avgDuration10Videos;
+        this.freequency = freequency;
+        this.isChannelVerify = isChannelVerify;
+        this.videos = videos;
     }
 
-    public String getUploader() {
-        return uploader;
+    public String getchannel() {
+        return channel;
     }
 
-    public void setUploader(String uploader) {
-        this.uploader = uploader;
+    public void setchannel(String channel) {
+        this.channel = channel;
     }
 
     public String getEpoch() {
@@ -52,6 +76,46 @@ public class Channel {
         this.playlist_count = playlist_count;
     }
 
+    public String getAvgLike10Videos() {
+        return avgLike10Videos;
+    }
+
+    public void setAvgLike10Videos(String avgLike10Videos) {
+        this.avgLike10Videos = avgLike10Videos;
+    }
+
+    public String getAvgView10Videos() {
+        return avgView10Videos;
+    }
+
+    public void setAvgView10Videos(String avgView10Videos) {
+        this.avgView10Videos = avgView10Videos;
+    }
+
+    public String getAvgComment10Videos() {
+        return avgComment10Videos;
+    }
+
+    public void setAvgComment10Videos(String avgComment10Videos) {
+        this.avgComment10Videos = avgComment10Videos;
+    }
+
+    public String getAvgDuration10Videos() {
+        return avgDuration10Videos;
+    }
+
+    public void setAvgDuration10Videos(String avgDuration10Videos) {
+        this.avgDuration10Videos = avgDuration10Videos;
+    }
+
+    public String getFreequency() {
+        return freequency;
+    }
+
+    public void setFreequency(String freequency) {
+        this.freequency = freequency;
+    }
+
     public ArrayList<Video> getVideos() {
         return videos;
     }
@@ -60,13 +124,35 @@ public class Channel {
         this.videos = videos;
     }
 
+    public String getChannel() {
+        return channel;
+    }
+
+    public void setChannel(String channel) {
+        this.channel = channel;
+    }
+
+    public String getIsChannelVerify() {
+        return isChannelVerify;
+    }
+
+    public void setIsChannelVerify(String isChannelVerify) {
+        this.isChannelVerify = isChannelVerify;
+    }
+
     @Override
     public String toString() {
         return "Channel{" +
-                "uploader='" + uploader + '\'' +
+                "channel='" + channel + '\'' +
                 ", epoch='" + epoch + '\'' +
                 ", channel_follower_count='" + channel_follower_count + '\'' +
                 ", playlist_count='" + playlist_count + '\'' +
+                ", avgLike10Videos='" + avgLike10Videos + '\'' +
+                ", avgView10Videos='" + avgView10Videos + '\'' +
+                ", avgComment10Videos='" + avgComment10Videos + '\'' +
+                ", avgDuration10Videos='" + avgDuration10Videos + '\'' +
+                ", freequency='" + freequency + '\'' +
+                ", isChannelVerify='" + isChannelVerify + '\'' +
                 ", videos=" + videos +
                 '}';
     }
