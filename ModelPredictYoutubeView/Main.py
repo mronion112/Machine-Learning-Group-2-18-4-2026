678bf87dbd55f808d094212e5e8b272e8b753768
mronion112 <user.name>
2026-04-15 10:57:56 +0700

    Finnal Model for Project

--- ModelPredictYoutubeView/Main.py
+++ ModelPredictYoutubeView/Main.py
@@ -4,14 +4,18 @@ import numpy as np
 from  sklearn.metrics import mean_squared_error
 from sklearn.linear_model import SGDRegressor, Ridge
 from sklearn.preprocessing import StandardScaler
+from sklearn.metrics import r2_score
+
 
 def readDataFile(filePath:str) -> pd.DataFrame:
-    dataFile = pd.read_csv(filePath)
+    dataFile = pd.read_csv("data/"+filePath)
 
     return dataFile
 
+# channel	epoch	followers	video_count	avg10View	avg10Like	avg10Comment	frequency	predict_view	predict_like	predict_comment	view
+
 def getXandY(dataFile: pd.DataFrame) -> tuple[np.ndarray, np.ndarray]:
-    X  = dataFile[["epoch", "followers", "video_count", "avg10View", "avg10Like", "avg10Comment", "avg10Duration", "frequency", "isVerify", "avgView", "avgLikes", "avgComments", "avgDuration"]].values
+    X  = dataFile[["epoch", "followers", "video_count", "avg10View", "avg10Like", "avg10Comment", "frequency", "predict_view", "predict_like", "predict_comment"]].values
     Y = dataFile[["view"]].values.flatten()
 
     return X, Y
@@ -62,66 +66,64 @@ def drawMatLab(historyE: np.ndarray, interation: int, hx: np.ndarray, Y: np.ndar
 
 if __name__ == "__main__":
     filePath: str = "Valorant.csv"
-    interation: int = 1000
-    learningRate: int = 0.0000001
-    regularization: float = 0.01
+    interation: int = 10000
+    learningRate: int = 0.000000001
+    regularization: float = 0.001
 
     dataFile: pd.DataFrame = readDataFile(filePath)
 
-
     X, Y = getXandY(dataFile)
-
     scalerX = StandardScaler()
     X = scalerX.fit_transform(X)
-
-
-    scalerY = StandardScaler()
-    Y = scalerY.fit_transform(Y.reshape(-1,1)).flatten()
+    
+    # scalerY = StandardScaler()
+    # Y = scalerY.fit_transform(Y.reshape(-1,1)).flatten()
 
     n = len(X)
-    XTrain = X[:n-10]
-    YTrain = Y[:n-10]
+    XTrain = X[:n-50]
+    YTrain = Y[:n-50]
 
-    XTest = X[n-10:]
-    YTest = Y[n-10:]
+    XTest = X[n-50:]
+    YTest = Y[n-50:]
 
     linearModel = SGDRegressor(
         penalty='l2',
         alpha=regularization,
         max_iter=1,
-        learning_rate='constant',
-        eta0=learningRate,
+        learning_rate='optimal',
+        # eta0=learningRate,
         warm_start=True
     )
 
     historyE = np.zeros(interation)
     finalTheta = np.zeros(X.shape[1])
-    finalHx = np.zeros(X.shape[0])
+    hxFinal = np.zeros(X.shape[0])
     for inter in range(interation):
         linearModel.partial_fit(XTrain, YTrain) 
         # linearModel.fit(XTrain, YTrain)
         hx = linearModel.predict(XTrain)
-        E = mean_squared_error(YTrain, hx)
+        E = mean_squared_error(YTrain, hx)/2
         historyE[inter] = E
 
-        print(E)
+        print(f"{inter}/{interation} : {E}")
 
         if inter == interation-1:
             finalTheta = linearModel.coef_
-            finalHx = hx
+            hxFinal = hx
 
-    print(f"The finnal theta is : {finalTheta}")
-    print(f"The finnal E is : {historyE[interation-1]}")
+    print(f"The final theta is : {finalTheta}")
+    print(f"The final E is : {historyE[interation-1]}")
 
-
-     
-
-    # linearModel.fit(XTest, YTest)
     hxTest  = linearModel.predict(XTest)
     ETEst = mean_squared_error(YTest, hxTest)
 
-
     print(f"Test E is : {ETEst}")
 
-    drawMatLab(historyE, interation, hx, YTrain, hxTest, YTest)
+    r2_train = r2_score(YTrain, hxFinal)
+    r2_test = r2_score(YTest, hxTest)
+
+    print(f"R2 Train: {r2_train*100}")
+    print(f"R2 Test: {r2_test*100}")
+
+    drawMatLab(historyE, interation, hxFinal, YTrain, hxTest, YTest)
 
